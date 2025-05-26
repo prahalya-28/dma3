2f517b5cc22b84ea235657aaeb55a52ebfb9c9c4
Prahalya <23z354@psgtech.ac.in>
2025-05-26 16:15:08 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -36,8 +36,9 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
+  origin: "https://prahalya-28.github.io",
   origin: "https://prahalya-28.github.io/dma3/", // Allow all origins for development
-  //origin: "https://prahalya-28.github.io", // Allow all origins for development
+   // Allow all origins for development
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
