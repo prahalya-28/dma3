fc6bf613f8d17278c99b74552da5b7c9d79d6083
Prahalya <23z354@psgtech.ac.in>
2025-05-26 17:14:56 +0530

    final 4.0

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000||10000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`Test endpoint: https://dma-qhwn.onrender.com:${PORT}/test`);
